9490d3a0963a727b15ed15469d19159badeac01f
wembrey <git@willembrey.com>
2019-10-12 18:05:00 +0100

    Update creds

--- python/creds.py
+++ python/creds.py
@@ -1,5 +1,5 @@
 # you can leave these blank because we will check for them
-hostname = '192.168.0.100'
-username = 'admin'
-password = 'honda600'
+hostname = ''
+username = ''
+password = ''
 apikey = ''
