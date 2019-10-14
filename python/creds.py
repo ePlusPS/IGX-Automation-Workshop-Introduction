9795771f280ec0e1659eff712ec7f865123ed4bb
wembrey <git@willembrey.com>
2019-10-14 15:25:06 +0100

    Update creds

---
+++ python/creds.py
@@ -0,0 +1,7 @@
+# you can leave these blank because we will check for them
+# if apikey is present, the others are not required
+apikey = ''
+
+hostname = '3.218.191.224'
+username = 'apiuser'
+password = 'apiuser'
